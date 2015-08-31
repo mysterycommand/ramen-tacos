2b6c3ebe5c8f02f9f0e16d796319a8849ecc49e8
Matt Hayes <mhayes@tumblr.com>
2015-08-31 14:51:32 -0700

    Create the MoviesViewController, and assign it to the main view's class.

---
+++ Ramen Tacos/MoviesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MoviesViewController.swift
+//  Ramen Tacos
+//
+//  Created by Matt Hayes on 8/31/15.
+//  Copyright (c) 2015 Mystery Command. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
