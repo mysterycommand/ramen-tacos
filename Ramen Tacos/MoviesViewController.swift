f89a660944be9651cdc66b9a8a8511bca864249c
Matt Hayes <mhayes@tumblr.com>
2015-08-31 16:38:49 -0700

    Programmatical table view (and data from that Gist).

--- Ramen Tacos/MoviesViewController.swift
+++ Ramen Tacos/MoviesViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var tableView: UITableView!
     
-    var tf: UITextField!
+    var movies: [NSDictionary]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.tf = UITextField(frame: CGRect(x: 10.0, y: 30.0, width: 300.0, height: 30.0))
-        self.tf.borderStyle = UITextBorderStyle.RoundedRect
-        self.view.addSubview(self.tf)
+        self.tableView = UITableView()
+        self.tableView.frame = CGRect(x: 0, y: 0, width: 320, height: 568)
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
         
-        println(self.view.subviews)
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MovieCell")
+        
+        self.view.addSubview(self.tableView)
+        
+        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")!
+        let req = NSURLRequest(URL: url)
+
+        NSURLConnection.sendAsynchronousRequest(req, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary
+            if let json = json {
+                self.movies = json["movies"] as! [NSDictionary]
+                self.tableView.reloadData()
+                println(self.movies)
+            }
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -29,6 +46,22 @@ class MoviesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let movies = self.movies {
+            return movies.count
+        }
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let movie = self.movies![indexPath.row]
+        
+        cell.textLabel?.text = movie["title"] as? String
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
