f8bd568645bcc92b3699d0e7c457c4df2169aa57
Matt Hayes <mhayes@tumblr.com>
2015-08-31 18:44:57 -0700

    Add a DetailsVC.

---
+++ Ramen Tacos/DetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailsViewController.swift
+//  Ramen Tacos
+//
+//  Created by Matt Hayes on 8/31/15.
+//  Copyright (c) 2015 Mystery Command. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    var posterView: UIImageView!
+    var titleLabel: UILabel!
+    var synopsisLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+}
